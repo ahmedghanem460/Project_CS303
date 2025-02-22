4f765d4e18e96f3b3476cd554c7124709975e359
ahmedghanme460 <ahmedmohamedghanem083@gmai.com>
2025-02-22 18:42:38 +0200

    edited

---
+++ app/about.tsx
@@ -0,0 +1,61 @@
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+
+const teamMembers = [
+  { name: 'Ahmed mohamed',   id: '2227009', email: 'ahmedmohamedghanem083@gmail.com' },
+  { name: 'Yousef hesham',   id: '2227049', email: 'he was sleep so i can not know from him' },
+  { name: 'Kareem abdullah', id: '2227252', email:'bambokareem72@gmail.com'},
+  { name: 'Ziad ahmed',      id: '2227298', email: 'Ziada9703@gmail.com'},
+  { name: 'Ahmed aymen',     id: '2227295', email: 'ahmedaymanmido307@gmail.com'},
+  { name: 'Kareem waheed',   id: '2127238', email: 'karimwaheed263@gmail.com'},
+  { name: 'Amr ahmed',       id: '2227009', email: 'amrahmed757574@gmail.com'},
+  { name: ' Mohamed yasser', id: '2227283', email: 'mohamedyasserelhwary813@gmail.com'},
+
+];
+export default function AboutScreen() {
+  return (
+    <ScrollView style={styles.container}>
+      <Text style={styles.title}>Team members </Text>
+      {teamMembers.map((member, index) => (
+        <View key={index} style={styles.card}>
+          <Text style={styles.name}>{member.name}</Text>
+          <Text style={styles.info}>ID: {member.id}</Text>
+          {member.email ? <Text style={styles.info}>📧 {member.email}</Text> : null}
+        </View>
+      ))}
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#fff',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  card: {
+    backgroundColor: '#333',
+    padding: 15,
+    borderRadius: 8,
+    marginBottom: 10,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#f8f8f8',
+  },
+  info: {
+    color: '#ccc',
+    fontSize: 14,
+  },
+  link: {
+    color: '#1e90ff',
+    textDecorationLine: 'underline',
+  },
+});
---
+++ app/index.tsx
@@ -0,0 +1,52 @@
+import { View, StyleSheet, Text } from 'react-native';
+import { Image } from 'expo-image';
+
+const PlaceholderImage = require('@/assets/images/photo.png');
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+      We are a team of passionate developers working on this exciting project
+      our goal is to create an app about Educational platform
+      </Text>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
+      <Text style={styles.text}>
+       you should know that We will do all we have to improve you to be able to take Full Marks
+      </Text>
+      <Text style={styles.text}>
+      let me tell you our Team
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20, 
+  },
+  text: {
+    color: 'white',
+    fontSize: 18,
+    marginBottom: 10, 
+    textAlign: 'center',
+  },
+  imageContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 10, 
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
+
+
